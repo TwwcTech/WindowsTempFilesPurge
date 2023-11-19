b17333adfea7f262fd3b94c4be1c0c940e12cf98
twwct <twwc.tech@gmail.com>
2023-11-18 17:37:07 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsTempFilesPurge
+{
+    partial class TempPurgeForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TempPurgeMenu = new MenuStrip();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            documentationToolStripMenuItem = new ToolStripMenuItem();
+            TempPurgeMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // TempPurgeMenu
+            // 
+            TempPurgeMenu.ImageScalingSize = new Size(24, 24);
+            TempPurgeMenu.Items.AddRange(new ToolStripItem[] { helpToolStripMenuItem });
+            TempPurgeMenu.Location = new Point(0, 0);
+            TempPurgeMenu.Name = "TempPurgeMenu";
+            TempPurgeMenu.Size = new Size(562, 33);
+            TempPurgeMenu.TabIndex = 0;
+            TempPurgeMenu.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem, documentationToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(65, 29);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(270, 34);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // documentationToolStripMenuItem
+            // 
+            documentationToolStripMenuItem.Name = "documentationToolStripMenuItem";
+            documentationToolStripMenuItem.Size = new Size(270, 34);
+            documentationToolStripMenuItem.Text = "Documentation";
+            documentationToolStripMenuItem.Click += documentationToolStripMenuItem_Click;
+            // 
+            // TempPurgeForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(562, 450);
+            Controls.Add(TempPurgeMenu);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MainMenuStrip = TempPurgeMenu;
+            MaximizeBox = false;
+            Name = "TempPurgeForm";
+            ShowIcon = false;
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Purge Temporary Files";
+            Load += TempPurgeForm_Load;
+            TempPurgeMenu.ResumeLayout(false);
+            TempPurgeMenu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip TempPurgeMenu;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem documentationToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace WindowsTempFilesPurge
+{
+    public partial class TempPurgeForm : Form
+    {
+        public TempPurgeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TempPurgeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void documentationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
