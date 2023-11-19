e1a9716a23c3e00481dcd791ce7cac94d0811eb2
twwct <twwc.tech@gmail.com>
2023-11-18 22:03:23 -0700

    backend connected

--- Frontend/About/AboutForm.cs
+++ Frontend/About/AboutForm.cs
@@ -9,7 +9,14 @@
 
         private void AboutForm_Load(object sender, EventArgs e)
         {
-            AboutLabel.Text = ""; // Complete the label text
+            AboutLabel.Text = "Windows Temp Files Purge is a utility designed to help users maintain " +
+                "\ntheir digital hygiene by purging unnecessary temporary files that often clutter up storage space. " +
+                "\n\n\nOne of the key features of TempFilePurge is its ability to also purge files from the Downloads folder. You have " +
+                "the option to select this feature, which can be particularly useful for those who frequently download files " +
+                "and forget to delete them later.";
+            AboutLabel.AutoSize = false;
+            AboutLabel.TextAlign = ContentAlignment.MiddleLeft;
+            AboutLabel.Padding = new Padding(20, 0, 0, 0);
         }
     }
 }
