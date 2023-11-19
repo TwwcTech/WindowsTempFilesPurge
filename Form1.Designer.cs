16db2c1b51f9177eb57b233bc0d96bf9c204bc10
twwct <twwc.tech@gmail.com>
2023-11-18 17:43:16 -0700

    button, checkbox and progress bar added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,9 @@
             helpToolStripMenuItem = new ToolStripMenuItem();
             aboutToolStripMenuItem = new ToolStripMenuItem();
             documentationToolStripMenuItem = new ToolStripMenuItem();
+            PurgeFilesButton = new Button();
+            DownloadsCheckbox = new CheckBox();
+            progressBar1 = new ProgressBar();
             TempPurgeMenu.SuspendLayout();
             SuspendLayout();
             // 
@@ -55,22 +58,55 @@
             // aboutToolStripMenuItem
             // 
             aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            aboutToolStripMenuItem.Size = new Size(270, 34);
+            aboutToolStripMenuItem.Size = new Size(237, 34);
             aboutToolStripMenuItem.Text = "About";
             aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
             // 
             // documentationToolStripMenuItem
             // 
             documentationToolStripMenuItem.Name = "documentationToolStripMenuItem";
-            documentationToolStripMenuItem.Size = new Size(270, 34);
+            documentationToolStripMenuItem.Size = new Size(237, 34);
             documentationToolStripMenuItem.Text = "Documentation";
             documentationToolStripMenuItem.Click += documentationToolStripMenuItem_Click;
+            // 
+            // PurgeFilesButton
+            // 
+            PurgeFilesButton.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PurgeFilesButton.Location = new Point(89, 153);
+            PurgeFilesButton.Name = "PurgeFilesButton";
+            PurgeFilesButton.Size = new Size(381, 60);
+            PurgeFilesButton.TabIndex = 1;
+            PurgeFilesButton.Text = "Purge Temporary Files";
+            PurgeFilesButton.UseVisualStyleBackColor = true;
+            PurgeFilesButton.Click += PurgeFilesButton_Click;
+            // 
+            // DownloadsCheckbox
+            // 
+            DownloadsCheckbox.AutoSize = true;
+            DownloadsCheckbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            DownloadsCheckbox.Location = new Point(106, 244);
+            DownloadsCheckbox.Name = "DownloadsCheckbox";
+            DownloadsCheckbox.Size = new Size(350, 34);
+            DownloadsCheckbox.TabIndex = 2;
+            DownloadsCheckbox.Text = "Include files in Downloads";
+            DownloadsCheckbox.UseVisualStyleBackColor = true;
+            DownloadsCheckbox.CheckedChanged += DownloadsCheckbox_CheckedChanged;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 389);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(538, 34);
+            progressBar1.TabIndex = 3;
             // 
             // TempPurgeForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(562, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(DownloadsCheckbox);
+            Controls.Add(PurgeFilesButton);
             Controls.Add(TempPurgeMenu);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             MainMenuStrip = TempPurgeMenu;
@@ -93,5 +129,8 @@
         private ToolStripMenuItem helpToolStripMenuItem;
         private ToolStripMenuItem aboutToolStripMenuItem;
         private ToolStripMenuItem documentationToolStripMenuItem;
+        private Button PurgeFilesButton;
+        private CheckBox DownloadsCheckbox;
+        private ProgressBar progressBar1;
     }
 }
