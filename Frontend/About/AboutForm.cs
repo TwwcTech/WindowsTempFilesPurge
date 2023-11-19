46ac999bc395e9c9660b0ff7141c7681297aabfc
twwct <twwc.tech@gmail.com>
2023-11-18 21:11:08 -0700

    backend connected

---
+++ Frontend/About/AboutForm.cs
@@ -0,0 +1,15 @@
+﻿namespace WindowsTempFilesPurge.Frontend.About
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
