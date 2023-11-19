3d88a26071ccf3983f600bd28904fa3afbbf22bf
twwct <twwc.tech@gmail.com>
2023-11-18 21:11:02 -0700

    form created

---
+++ Frontend/About/AboutForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WindowsTempFilesPurge.Frontend.About
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AboutLabel = new Label();
+            SuspendLayout();
+            // 
+            // AboutLabel
+            // 
+            AboutLabel.AutoSize = true;
+            AboutLabel.Dock = DockStyle.Fill;
+            AboutLabel.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            AboutLabel.Location = new Point(0, 0);
+            AboutLabel.Name = "AboutLabel";
+            AboutLabel.Size = new Size(99, 34);
+            AboutLabel.TabIndex = 0;
+            AboutLabel.Text = "label1";
+            // 
+            // AboutForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(AboutLabel);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AboutForm";
+            ShowIcon = false;
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "About";
+            Load += AboutForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label AboutLabel;
+    }
+}
